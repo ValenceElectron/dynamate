d6440113cd3da106cb3593b24eb98da3c4677889
ValenceElectron <TheLastElectronSPDF@gmail.com>
2023-11-20 16:22:21 -0800

    Renamed init() to initGLFW() and moved more glfw calls into initGLFW() to clean up main()

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,17 @@ int width, height;
 
 GLFWwindow* window;
 
-void init() {
+void initGLFW() {
     std::cout << "Initializing...\n";
     if (!glfwInit()) {exit(EXIT_FAILURE);}
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
 
     int count;
     GLFWmonitor** monitors = glfwGetMonitors(&count);
     const GLFWvidmode* videoMode = glfwGetVideoMode(monitors[0]);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
 
     // width = 75% of the screen
     windowWidth = static_cast<int>(videoMode->width / 1.5);
@@ -28,31 +28,25 @@ void init() {
 
     glfwGetMonitorPos(monitors[0], &monitorX, &monitorY);
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-    
     window = glfwCreateWindow(600, 600, "Dynamate", NULL, NULL);
-
     glfwMakeContextCurrent(window);
     glfwSetWindowPos(window, monitorX + (videoMode->width - windowWidth) / 2, monitorY + (videoMode->height - windowHeight) / 2);
-
     glfwShowWindow(window);
 
     if(glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
     glfwSwapInterval(1);
+
+    // comment following line to see the border of window
+    glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
 }
 
 int main(void) {
-    init();
-    //init(window);
-
+    initGLFW();
     std::cout << "Initializing rendering pipeline...\n";
     Draw draw(window, width, height);
 
-    // comment following line to see the border of window
-    //glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
-    
     std::cout << "Beginning main program loop...\n";
     while(!glfwWindowShouldClose(window)) {
-        //std::cout << "drawing\n";
         draw.startDrawing(glfwGetTime());
         glfwSwapBuffers(window);
         glfwPollEvents();
