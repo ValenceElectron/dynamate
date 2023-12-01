5c232557848f55e1e890d0ea42aa195ce23a5ded
ValenceElectron <TheLastElectronSPDF@gmail.com>
2023-12-01 00:42:02 -0800

    Project is now licensed under the MPL v 2.0

--- src/render/objects/UserInterfaceElement.cpp
+++ src/render/objects/UserInterfaceElement.cpp
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "UserInterfaceElement.hpp"
 
 UserInterfaceElement::UserInterfaceElement(std::string objType, float *pos, float scale, float *vertices, int numberOfVertices, double *buttonBounds)
